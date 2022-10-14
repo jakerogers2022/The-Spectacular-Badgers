0120b524247c7534bdb77c3dbd38976dcb72b5ab
kellyxj <52384254+kellyxj@users.noreply.github.com>
2022-10-13 21:38:35 -0500

    Update src.sql

--- Checkpoint1/src.sql
+++ Checkpoint1/src.sql
@@ -10,22 +10,68 @@ ON arrest_f.district = arrest_t.district
 ORDER BY proportion_arrest;
 
 
--- 2. How many search warrants are executed per police unit normalized by unit size?
--------- 2a
-SELECT policeunit_id, CAST(Count(*) AS float) / officer_count as num_warrants
-FROM data_searchwarrant
-INNER JOIN (SELECT Count(first_name) as officer_count, last_unit_id FROM data_officer group by last_unit_id) as a ON a.last_unit_id = data_searchwarrant.policeunit_id
-GROUP BY policeunit_id, a.officer_count
-ORDER BY num_warrants;
 
--------- 2b
+--Question 2: Does racial bias play a role in the outcomes of search warrants?
+
+SELECT a.name AS district_name, race, SUM("count") population
+FROM data_racepopulation dr JOIN data_area a ON a.id =dr.area_id
+WHERE area_type='police-districts'
+GROUP BY race, area_id, a.name
+ORDER BY district_name;
+
+SELECT a.name AS district_name, SUM("count") population, black_pop, white_pop
+FROM data_racepopulation dr
+    JOIN data_area a ON a.id =dr.area_id
+    JOIN (
+        SELECT a2.name, SUM("count") black_pop
+        FROM data_racepopulation dr2 JOIN data_area a2 ON a2.id=dr2.area_id
+        WHERE area_type = 'police-districts' AND race='Black'
+        GROUP BY a2.name
+    ) subtable ON a.name = subtable.name
+    JOIN (
+        SELECT a2.name, SUM("count") white_pop
+        FROM data_racepopulation dr2 JOIN data_area a2 ON a2.id=dr2.area_id
+        WHERE area_type = 'police-districts' AND race='White'
+        GROUP BY a2.name
+    ) subtable2 ON a.name = subtable2.name
+WHERE area_type='police-districts'
+GROUP BY district_name, black_pop,white_pop
+ORDER BY district_name;
+
+SELECT a.name AS district_name, COUNT(DISTINCT s.id) AS search_warrant_cnt, population, white_pop, black_pop
+FROM data_area a
+   JOIN data_searchwarrant s ON ST_INTERSECTS(a.polygon, s.point)
+   JOIN data_racepopulation dr on a.id = dr.area_id
+    JOIN (
+        SELECT a2.name, SUM("count") black_pop
+        FROM data_racepopulation dr2 JOIN data_area a2 ON a2.id=dr2.area_id
+        WHERE area_type = 'police-districts' AND race='Black'
+        GROUP BY a2.name
+    ) subtable ON a.name = subtable.name
+    JOIN (
+        SELECT a2.name, SUM("count") white_pop
+        FROM data_racepopulation dr2 JOIN data_area a2 ON a2.id=dr2.area_id
+        WHERE area_type = 'police-districts' AND race='White'
+        GROUP BY a2.name
+    ) subtable2 ON a.name = subtable2.name
+   JOIN (
+       SELECT a2.name, SUM("count") AS population
+       FROM data_racepopulation dr2 JOIN data_area a2 ON a2.id=dr2.area_id
+       WHERE area_type = 'police-districts'
+       GROUP BY a2.name
+   ) subtable3 ON subtable3.name = a.name
+WHERE area_type='police-districts'
+GROUP BY district_name, population, white_pop, black_pop
+ORDER BY search_warrant_cnt DESC;
+
+-------- 3. What is the geographic extent of search warrants executed by each police unit?
 SELECT avg(x.num_districts), max(x.num_districts), min(x.num_districts)
 FROM (
    SELECT Count(DISTINCT district) as num_districts, policeunit_id FROM data_searchwarrant GROUP BY policeunit_id
     ) x;
 
 
--- 3. What proportion of lawsuits list unlawful search/seizure as the primary cause?
+-- 4. What proportion of lawsuits list unlawful search/seizure as the primary cause?
 SELECT COUNT(*)
 FROM lawsuit_lawsuit;
 
@@ -34,20 +80,36 @@ FROM lawsuit_lawsuit
 GROUP BY primary_cause
 HAVING COUNT(*) > 1;
 
--- 4. What fraction of TRRs are located at residences?
-SELECT COUNT(*) FROM trr_trr;
+-- 5. Of the complaints categorized as improper search, what proportion alleged search of premise without a warrant?
+--question 5: Of the complaints categorized as improper search, what proportion alleged search of premise without a warrant?
+SELECT category, allegation_name, COUNT(DISTINCT allegation_id) cr_count
+FROM data_allegationcategory c JOIN data_officerallegation d
+ON c.id = d.allegation_category_id
+WHERE category_code ILIKE '03_'
+GROUP BY category, allegation_name;
 
-SELECT COUNT(*) FROM trr_trr WHERE 'Residence' = location;
+SELECT COUNT(DISTINCT allegation_id) cr_count
+FROM data_allegationcategory c JOIN data_officerallegation d
+ON c.id = d.allegation_category_id
+WHERE category_code ILIKE '03_';
 
--- 5. What fraction of lawsuits have a location listed?
-SELECT COUNT(*) FROM lawsuit_lawsuit;
+SELECT category, allegation_name, COUNT(DISTINCT allegation_id) cr_count
+FROM data_allegationcategory c JOIN data_officerallegation d
+ON c.id = d.allegation_category_id
+WHERE category_code ILIKE '03C'
+GROUP BY category, allegation_name;
 
-SELECT COUNT(*) FROM lawsuit_lawsuit WHERE location <> '';
+SELECT DISTINCT d.allegation_id, category_code, cr_text
+FROM data_allegationcategory c
+    JOIN data_officerallegation d ON c.id = d.allegation_category_id
+    JOIN data_allegation da ON d.allegation_id = da.crid
+WHERE category_code = '03C' AND cr_text IS NOT NULL;
 
-SELECT COUNT(*) FROM lawsuit_lawsuit WHERE add1 <> '' AND add2 <> '';
-
-SELECT COUNT(*) FROM lawsuit_lawsuit WHERE point IS NOT NULL;
-
+SELECT DISTINCT d.allegation_id, category_code, summary
+FROM data_allegationcategory c
+    JOIN data_officerallegation d ON c.id = d.allegation_category_id
+    JOIN data_allegation da ON d.allegation_id = da.crid
+WHERE category_code = '03C' AND summary<>'';
 
 
 
